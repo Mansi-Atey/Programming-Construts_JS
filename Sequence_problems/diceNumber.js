40d6093cfc14b06bec8cb07e4b112e36253e42e8
Mansi-Atey <mansiatey31@gmail.com>
2021-04-23 20:37:13 +0530

    [mansi]Added: problems using case statements

--- Sequence_problems/diceNumber.js
+++
@@ -1,2 +0,0 @@
-let digit = Math.floor(Math.random() * 6) +1;
-console.log("single digit" + digit );
--- Sequence_problems/singleDigit.js
+++
@@ -1,2 +0,0 @@
-let digit = Math.floor(Math.random() * 10);
-console.log("single digit" + digit );
